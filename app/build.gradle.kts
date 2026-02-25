5743a752dc68b86c229e2b4d618472bb7e26e1bf
Jacob Briggs <github@jacobbriggs.com>
2026-02-25 23:37:41 +0000

    Increment for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.wineselector.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
     }
