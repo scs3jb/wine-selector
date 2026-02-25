5f4a679127bf77156d929ad8a77ac87742152407
Jacob Briggs <github@jacobbriggs.com>
2026-02-25 01:46:39 +0000

    Fixing SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.wineselector.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
     }
